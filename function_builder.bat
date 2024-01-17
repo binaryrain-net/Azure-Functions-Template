ae39adc3221fa0d668f0e50eef8e5e6819f60e50
Marcel T.O <marcel@turobin-ort.at>
2024-01-17 17:44:17 +0100

    Add Azure Functions template files

---
+++ function_builder.bat
@@ -0,0 +1,33 @@
+@echo off
+
+REM Change the directory to the current directory
+cd /d "%~dp0"
+
+REM Create a virtual environment
+echo Creating virtual environment...
+call python -m venv .venv
+echo Virtual environment created successfully.
+
+REM Ask the user to enter a Function Name
+set /p functionName=Enter the Function Name: 
+
+REM Create the "Azure Functions" folder and move to the created folder
+mkdir "Azure Functions"
+cd "Azure Functions\"
+
+REM Create an Azure Function in python
+echo Creating Azure Function...
+func new %functionName% --worker-runtime python --model v2 --template "Http Trigger" --name %functionName% --authlevel function
+echo Azure Function created successfully.
+
+REM Change the directory to the Azure Functions folder
+cd "%~dp0\Azure Functions\%functionName%"
+
+REM Remove the content of the requirements.txt file and add needed requirements
+echo. > requirements.txt
+echo azure-functions >> requirements.txt
+echo azure-identity >> requirements.txt
+echo azure-storage-blob >> requirements.txt
+echo pandas >> requirements.txt
+echo python-dotenv >> requirements.txt
+echo pydantic >> requirements.txt
